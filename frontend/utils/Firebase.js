04fadb2f2a2acf645b4f8e90a755deb530072719
Anas <mranasmirza1@gmail.com>
2026-02-05 13:26:49 +0530

    bearandbond

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,11 +2,11 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "bearandbond.firebaseapp.com",
-  projectId: "bearandbond",
-  storageBucket: "bearandbond.firebasestorage.app",
-  messagingSenderId: "588174887971",
-  appId: "1:588174887971:web:080d6b62a4d31c35ecf596"
+  authDomain: "bond-ee03b.firebaseapp.com",
+  projectId: "bond-ee03b",
+  storageBucket: "bond-ee03b.firebasestorage.app",
+  messagingSenderId: "1021994570924",
+  appId: "1:1021994570924:web:4555cd534661809dc43b09"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
